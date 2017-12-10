b33716bdc7b9ec1640c004184b96f3841618668c
Ricky Chien <ricky060709@gmail.com>
2017-12-10 13:35:59 +0800

    Fix webpack not found issue on heroku

--- server/index.js
+++ server/index.js
@@ -4,25 +4,16 @@ const convert = require('koa-convert');
 const Router = require('koa-router');
 const send = require('koa-send');
 const websockify = require('koa-websocket');
-const webpack = require('webpack');
-const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware');
 
 const Hangout = require('./hangout');
 const config = require('../webpack.config');
 
-const compiler = webpack(config);
+const isDev = process.env.NODE_ENV !== "production";
 const app = websockify(new Koa());
 const router = new Router();
 const wsRouter = new Router();
 const PORT = 3000;
 
-const isDev = process.env.NODE_ENV !== "production";
-const devMiddlewareConfig = {
-  publicPath: config.output.publicPath,
-  stats: {
-    colors: true,
-  },
-};
 
 router.get('/*', async (ctx) => {
   await send(ctx, ctx.path, { index: 'index.html', root: `${config.output.path}` });
@@ -40,6 +31,17 @@ app.ws
   .use(wsRouter.routes());
 
 if (isDev) {
+  const webpack = require('webpack');
+  const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware');
+
+  const devMiddlewareConfig = {
+    publicPath: config.output.publicPath,
+    stats: {
+      colors: true,
+    },
+  };
+  const compiler = webpack(config);
+
   app
     .use(convert(devMiddleware(compiler, devMiddlewareConfig)))
     .use(convert(hotMiddleware(compiler)));
