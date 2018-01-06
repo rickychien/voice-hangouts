1a94c40e0915fcac44ea18bcf4c0d84c59944968
Ricky Chien <ricky060709@gmail.com>
2018-01-06 19:44:21 +0800

    Apply linter to ./*.js files

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
-const webpack = require('webpack');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
-const isDev = process.env.NODE_ENV !== 'production';
+const isDev = process.env.NODE_ENV !== 'production'
 
 module.exports = {
   entry: {
     client: [
       './client/index.jsx',
-      isDev && 'webpack-hot-middleware/client',
-    ].filter((file) => file),
+      isDev && 'webpack-hot-middleware/client'
+    ].filter((file) => file)
   },
   output: {
     path: `${__dirname}/public`,
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: '/'
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify(isDev ? 'development' : 'production'),
-      },
+        NODE_ENV: JSON.stringify(isDev ? 'development' : 'production')
+      }
     }),
     !isDev && new UglifyJSPlugin({
       uglifyOptions: {
         beautify: false,
         ecma: 6,
         compress: true,
-        comments: false,
-      },
-    }),
+        comments: false
+      }
+    })
   ].filter((file) => file),
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         use: 'babel-loader',
-        exclude: /node_modules/,
+        exclude: /node_modules/
       },
       {
         test: /\.(gif|svg|png|ttf|eot|jpe?g|woff2?)$/,
-        use: 'url-loader',
+        use: 'url-loader'
       },
       {
         test: /\.css$/,
@@ -55,12 +55,12 @@ module.exports = {
             options: {
               modules: true,
               importLoaders: 1,
-              localIdentName: '[path][name]_[local]__[hash:base64:5]',
-            },
-          },
-        ],
-      },
-    ],
+              localIdentName: '[path][name]_[local]__[hash:base64:5]'
+            }
+          }
+        ]
+      }
+    ]
   },
-  devtool: isDev && 'source-map',
-};
+  devtool: isDev && 'source-map'
+}
